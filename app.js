a63a29a184a5a16070aa1378d34d72b0cf956cae
frdgseb <danildolgov735@gmail.com>
2024-03-09 06:49:24 +0500

    versoin 2

--- app.js
+++ app.js
@@ -1,9 +1,14 @@
 const firstNumber = document.getElementById('firstNumber');
 const secondNumber = document.getElementById('secondNumber');
 const plus = document.getElementById('plus');
+const minus = document.getElementById('minus');
 const result = document.getElementById('result');
 
 plus.onclick = () => {
     let sum = Number(firstNumber.value) + Number(secondNumber.value);
+    result.textContent = sum;
+}
+minus.onclick = () => {
+    let sum = Number(firstNumber.value) - Number(secondNumber.value);
     result.textContent = sum;
 }
